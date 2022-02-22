e65f429c463416a56c2617df9599fcf122ab10a6
ajmarrocco <ajmarrocco@gmail.com>
2022-02-22 00:27:04 -0500

    engineer test'

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,12 +1,12 @@
 const Engineer = require('../lib/Engineer.js');
 
-test('returns the github account', () => {
+test('returns the school name', () => {
     const engineer = new Engineer();
 
     expect(engineer.getGithub()).toEqual(expect.any(String));
 })
 
-test('returns the Engineer as string', () => {
+test('returns the intern as string', () => {
     const engineer = new Engineer();
 
     expect(engineer.getRole()).toEqual(expect.any(String));
