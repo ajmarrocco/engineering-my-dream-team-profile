78b51d2d85c9e03ffcc117816258db85a6d7ab5c
ajmarrocco <ajmarrocco@gmail.com>
2022-02-21 20:05:56 -0500

    add css file

--- src/page-template.js
+++ src/page-template.js
@@ -73,18 +73,17 @@ module.exports = templateData => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
         <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
     </head>
 
     <body>
-        <header>
-            <div class="container flex-row justify-space-between align-center py-3">
-                <h1 class="page-title text-secondary bg-dark py-2 px-3">Hey</h1>
-                <nav class="flex-row">
-                </nav>
+        <header class="bg-danger">
+            <div class="container justify-space-between align-center py-3">
+                <h1 class="page-title text-white py-2 px-3">My Team</h1>
             </div>
         </header>
         <main class="container my-5">
